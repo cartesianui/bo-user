1a39305052fc3b3a615640663e66ed94a1d802cc
Ali Kazmi <ali@kazmi.me>
2023-12-08 15:14:55 +0500

    Updates routing structure (#5)

--- src/lib/user-routing.module.ts
+++ src/lib/user-routing.module.ts
@@ -1,33 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { UserComponent } from './user.component';
 import { UsersComponent } from './ui/users.component';
 import { UserConfigurationComponent } from './ui/configuration/user-configuration.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: UserComponent,
+    component: UsersComponent,
     data: {
       title: 'Users'
-    },
-    children: [
-      {
-        path: '',
-        component: UsersComponent,
-        data: {
-          title: ''
-        }
-      },
-      {
-        path: 'configuration',
-        component: UserConfigurationComponent,
-        data: {
-          title: 'Configuration'
-        }
-      }
-    ]
+    }
+  },
+  {
+    path: 'configuration',
+    component: UserConfigurationComponent,
+    data: {
+      title: 'Configuration'
+    }
   }
 ];
 
